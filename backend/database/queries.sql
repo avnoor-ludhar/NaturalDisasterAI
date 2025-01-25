991345d0029f9dc073fc8f160b92e344db547bd7
avnoor-ludhar <ludhar1@uwindsor.ca>
2025-01-24 20:07:34 -0500

    updated queries.sql

--- backend/database/queries.sql
+++ backend/database/queries.sql
@@ -6,33 +6,6 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-
-CREATE TABLE chat_logs (
-    chat_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id),
-    chat_title VARCHAR(255),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE messages (
-    message_id SERIAL PRIMARY KEY,
-    chat_id INT REFERENCES chat_logs(chat_id),
-    sender VARCHAR(100) NOT NULL,
-    receiver VARCHAR(100),
-    message TEXT NOT NULL,
-    timestamp TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE images (
-    image_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id),
-    filename VARCHAR(255) NOT NULL,
-    items TEXT, 
-    image_data BYTEA NOT NULL, 
-    uploaded_at TIMESTAMP DEFAULT NOW()
-);
-
-
 CREATE TABLE messages (
     message_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),
