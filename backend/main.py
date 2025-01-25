f2d3db54cb2f9a25ec14169135911b26a1fa5fa2
avnoor-ludhar <ludhar1@uwindsor.ca>
2025-01-24 19:41:53 -0500

    base project complete, AI integration needed much more time

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from controllers.disasterDataUpload import router as disaster_data_router
-from controllers.nebius import router as nebius_router
 from controllers.authentication import router as authentication_router
 from database.database import get_connection
-from utils.pinecone_db import clear_index
 
 app = FastAPI()
 
+#origins 
 origins = [
   "http://localhost:5173", 
 ]
 
-# cors middleware is added
+# cors middleware is added to the server
 app.add_middleware(
   CORSMiddleware,
   allow_origins=origins,
@@ -21,18 +20,12 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
-
+# includes the routers for modularized programming, tags are for the /docs route
 app.include_router(disaster_data_router, prefix="/api", tags=["disaster_upload"])
-app.include_router(nebius_router, prefix="/api", tags=["nebius"])
 app.include_router(authentication_router, prefix="/api", tags=["authentication"])
 
 
-conn = get_connection()
-cursor = conn.cursor()
-
-
+#just a basic endpoint to test
 @app.get("/")
 def read_root():
   return {"message": "Hello, World!"}
-
-#clear_index()
